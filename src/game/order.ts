c907ef049285be925ae98a459188f9a452a08490
Matt Grau <matt.grau@gmail.com>
2019-05-13 00:08:47 +0200

    fix bug with order on new epoch

--- src/game/order.ts
+++ src/game/order.ts
@@ -1,27 +1,20 @@
-import { GameState } from "./game";
+import { GameState } from "./setup";
 
 const TurnOrder = {
-  // OPTIONAL:
-  // Override the initial value of playOrder.
-  // This is called at the beginning of the phase.
-  playOrder: (G: GameState, ctx) => {
-    return ctx.playOrder.filter(pos => G.players[pos].suns.length > 0);
-  },
-
   // Get the initial value of playOrderPos.
   // This is called at the beginning of the phase.
-  //   first: (G: GameState, ctx) => 0,
-  first: (G: GameState, ctx) =>
-    ctx.playOrderPos %
-    ctx.playOrder.filter(pos => G.players[pos].suns.length > 0).length,
+  first: (G: GameState, ctx) => ctx.playOrderPos,
 
   // Get the next value of playOrderPos.
   // This is called at the end of each turn.
   // The phase ends if this returns undefined.
-  //   next: (G: GameState, ctx) => (ctx.playOrderPos + 1) % ctx.numPlayers,
-  next: (G: GameState, ctx) =>
-    (ctx.playOrderPos + 1) %
-    ctx.playOrder.filter(pos => G.players[pos].suns.length > 0).length
+  next: (G: GameState, ctx) => {
+    const playOrder = Array.from(
+      { length: ctx.numPlayers },
+      (x, pos) => (pos + ctx.currentPlayer + 1) % ctx.numPlayers
+    );
+    return playOrder.find(pos => G.players[pos].suns.length > 0);
+  }
 };
 
 export default TurnOrder;

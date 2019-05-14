b6b8d3051f982fe051c787608802814a93c27d77
Matt Grau <matt.grau@gmail.com>
2019-05-14 11:04:34 +0200

    add end of epoch test to turn order

--- src/game/order.test.ts
+++ src/game/order.test.ts
@@ -2,6 +2,7 @@ import { Client } from "boardgame.io/client";
 import { Ra } from "./game";
 import Setup, { GameState } from "./setup";
 import { Tile, TileType } from "./tile";
+import { Players } from "./player";
 
 test("order 2 player", () => {
   const RaTest = {
@@ -129,4 +130,44 @@ test("order 3 player", () => {
   store = client.store.getState();
   expect(store.ctx.currentPlayer).toBe("1");
   expect(store.ctx.phase).toBe("Action");
+});
+
+test("order end of epoch", () => {
+  const RaTest = {
+    ...Ra,
+    setup: ctx => {
+      const players = Players([[2, 6], [3, 4], [5, 10]]);
+      const G: GameState = {
+        epoch: 1,
+        sun: 1,
+        ra: null,
+        players: players,
+        raTrack: [
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra },
+          <Tile>{ tileType: TileType.Ra }
+        ],
+        auctionTrack: [],
+        tiles: [<Tile>{ tileType: TileType.Ra }]
+      };
+      return G;
+    }
+  };
+
+  const client: any = Client({ numPlayers: 3, game: RaTest });
+
+  var store: { G: GameState; ctx: any };
+
+  store = client.store.getState();
+  expect(store.G.epoch).toBe(1);
+  expect(store.ctx.currentPlayer).toBe("0");
+
+  client.moves.draw();
+  store = client.store.getState();
+  expect(store.G.epoch).toBe(2);
+  expect(store.ctx.currentPlayer).toBe("2");
 });

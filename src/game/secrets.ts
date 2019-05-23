dfcbb42221704ab02c546b77c291196895c827e1
Matt Grau <matt.grau@gmail.com>
2019-05-23 11:34:28 -0600

    fix secrets for tiles in Multiplayer

--- src/game/secrets.ts
+++ src/game/secrets.ts
@@ -1,22 +1,23 @@
 import { GameState } from "./setup";
+import { Tile } from "./tile";
 
 export default function StripSecrets(G: GameState, playerID: string) {
   let secretG = { ...G };
 
   if (secretG.tiles !== undefined) {
-    delete secretG.tiles;
+    secretG.tiles = [<Tile>{}];
   }
 
-  if (secretG.players) {
-    secretG.players = secretG.players.map((player, index) => {
-      let tempPlayer = player;
-      if (index != parseInt(playerID)) {
-        delete tempPlayer.points;
-        delete tempPlayer.score;
-      }
-      return tempPlayer;
-    });
-  }
+  // if (secretG.players) {
+  //   secretG.players = secretG.players.map((player, index) => {
+  //     let tempPlayer = player;
+  //     if (index != parseInt(playerID)) {
+  //       delete tempPlayer.points;
+  //       delete tempPlayer.score;
+  //     }
+  //     return tempPlayer;
+  //   });
+  // }
 
   return secretG;
 }

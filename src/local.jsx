f7159137e83848f214de7daffe9d87c8ef366684
Matt Grau <matt.grau@gmail.com>
2019-06-05 08:53:09 +0200

    add a bad mcts ai

--- src/local.jsx
+++ src/local.jsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom";
 import { Client } from "boardgame.io/react";
 import { Ra } from "./game/game.ts";
 import RaBoard from "./board/board";
-
-// import "./board/css/lobby.css";
+import RaAI from "./game/ai";
 
 const RaClient = Client({
   numPlayers: 2,
   game: Ra,
   board: RaBoard,
-    // multiplayer: { server: "http://localhost:5000" },
+  ai: RaAI,
   debug: true
 });
 
